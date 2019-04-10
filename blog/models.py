644e399a1b9eba9a29c3064406c2fef75b3102fc
yakusevv <yakusevv@gmail.com>
2019-04-10 21:26:28 +0200

    post detail

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.shortcuts import reverse
 
 class Post(models.Model):
     title = models.CharField(max_length=150, db_index=True)
@@ -8,3 +9,6 @@ class Post(models.Model):
 
     def __str__(self):
         return '{}'.format(self.title)
+
+    def get_absolute_url(self):
+        return reverse('post_detail_url', kwargs={'slug': self.sluf})
--- blog/views.py
+++ blog/views.py
@@ -7,3 +7,8 @@ from .models import Post
 def posts_list(request):
     posts = Post.objects.all()
     return render(request, 'blog/index.html', context={'posts': posts})
+
+
+def post_detail(request, slug):
+    post = Post.objects.get(slug__iexact=slug)
+    return render(request, 'blog/post_detail.html', context={'post': post})
