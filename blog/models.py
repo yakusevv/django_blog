450d8e6f140521df22e74dbed75acb7a6a7ab8ba
yakusevv <yakusevv@gmail.com>
2019-04-10 23:12:33 +0200

    model Tag + Tag detail

--- blog/models.py
+++ blog/models.py
@@ -6,9 +6,21 @@ class Post(models.Model):
     slug = models.SlugField(max_length=150, unique=True)
     body = models.TextField(blank=True, db_index=True)
     date_pub = models.DateTimeField(auto_now_add=True)
+    tags = models.ManyToManyField('Tag', related_name='posts')
 
     def __str__(self):
         return '{}'.format(self.title)
 
     def get_absolute_url(self):
-        return reverse('post_detail_url', kwargs={'slug': self.sluf})
+        return reverse('post_detail_url', kwargs={'slug': self.slug})
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50, unique=True)
+
+    def __str__(self):
+        return '{}'.format(self.title)
+
+    def get_absolute_url(self):
+        return reverse('tag_detail_url', kwargs={'slug': self.slug})
