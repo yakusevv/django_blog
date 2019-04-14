106f7cf41316981950dac1ec5ccf313e8d6e783c
yakusevv <yakusevv@gmail.com>
2019-04-14 15:48:18 +0200

    Post create

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,7 @@ from django.views.generic import View
 
 from .models import Post, Tag
 from .utils import ObjectDetailMixin
-from .forms import TagForm
+from .forms import TagForm, PostForm
 
 def posts_list(request):
     posts = Post.objects.all()
@@ -38,3 +38,16 @@ class TagCreate(View):
             new_tag = bound_form.save()
             return redirect(new_tag)
         return render(request, 'blog/tag_create.html', context={'form': bound_form})
+
+
+class PostCreate(View):
+    def get(self, request):
+        form = PostForm()
+        return render(request, 'blog/post_create_form.html', context={'form': form})
+
+    def post(self, request):
+        bound_form = PostForm(request.POST)
+        if bound_form.is_valid():
+            new_post = bound_form.save()
+            return redirect(new_post)
+        return render(request, 'blog/post_create_form.html', context={'form': bound_form})
